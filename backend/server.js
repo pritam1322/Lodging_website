a829c6a26d754338464ef3dd6f5cb392c20e594f
PRITAM PRAKASH JATHAR <pritamjat98@gmail.com>
2024-04-12 23:32:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,21 @@ import bookRoutes from './routes/bookRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
+const cors = require('cors')
+
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
-
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
