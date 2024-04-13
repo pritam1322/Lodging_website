f892e37c767151256c918d5734aef63c3bc73297
PRITAM PRAKASH JATHAR <pritamjat98@gmail.com>
2024-04-13 10:17:18 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://lodging-website.vercel.app/',
         changeOrigin: true,
       },
     },
